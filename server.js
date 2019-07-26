febd75f1a37ffc47ba78b7e236e68326735aac82
German Nunez <germannunez@Germans-MacBook-Pro.local>
2019-07-26 11:14:59 -0700

    commented out express-session for now

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const session = require('express-session')
+// const session = require('express-session')
 const app = express();
 
 //require db
