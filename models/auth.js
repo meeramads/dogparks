8cbe0bdb9edd1167d11505fdddbe43bbd8fab82e
German Nunez <germannunez@Germans-MacBook-Pro.local>
2019-07-31 11:26:22 -0700

    added Auth model

---
+++ models/auth.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const authSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true}
+})
+
+const Auth = mongoose.model('Auth', authSchema);
+
+module.export = Auth;
